b9c8de22bacc54143315f719f69ba25f1df50353
Mark de Leon <mark_deleon@hotmail.com>
2023-09-17 21:35:19 -0400

    Updated Cypress configurations

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'wd81pv',
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
